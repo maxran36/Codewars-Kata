3280811848b82c758c7f1a14ebcf53f40b88fe2d
maxran36 <124910435+maxran36@users.noreply.github.com>
2023-04-14 19:36:29 +0000

    Last try of the day

--- findWordNum.js
+++ findWordNum.js
@@ -4,16 +4,15 @@
 //   let charCode;
 //   letterArray = x.split(" ");
 
-//   console.log(letterArray);
+//   console.log(typeof letterArray);
 //   for (let i = 0; i < x.length; i++) {
 //     if (x.charCodeAt(i) >= 97 && x.charCodeAt(i) <= 122) {
 //       charCode = x.charCodeAt(i) - 96;
 //       result += charCode;
 //     }
-//   }
 //   return result;
 // }
-// console.log(high("abc abcd"));
+// console.log(high("abc abcdz"));
 // const strArray = "aa aa";
 // let splitArray = strArray.split(" ");
 // let countArray = [];
@@ -36,6 +35,7 @@
 //   } else sum2 += num;
 // }, 0);
 // console.log(sum);
+
 // const letterValues = {
 //   A: 1,
 //   B: 2,
@@ -58,31 +58,49 @@
 // console.log(highestValue);
 
 // Assign numbers to letter
-function letterValues(char) {
-  return char.toUpperCase().charCodeAt(0) - 64;
-}
+// function letterValues(char) {
+//   return char.toUpperCase().charCodeAt(0) - 64;
+// }
 
-// Function to calculate the value of a word based on the sum of it's letter values
-function wordValue(word) {
-  let value = 0;
-  for (let i = 0; i < word.length; i++) {
-    value += letterValues(word.charAt(i));
-  }
-  return value;
-}
+// // Function to calculate the value of a word based on the sum of it's letter values
+// function wordValue(word) {
+//   let value = 0;
+//   for (let i = 0; i < word.length; i++) {
+//     value += letterValues(word.charAt(i));
+//   }
+//   return value;
+// }
 
-function findGreatesValue(words) {
-  let greatestWord = null;
-  let greatestValue = 0;
-  for (let i = 0; i < words.length; i++) {
-    const value = wordValue(words[i]);
-    if (value > greatestValue) {
-      greatestValue = value;
-      greatestWord = words[i];
-    }
-  }
-  return greatestWord;
+// function findGreatesValue(words) {
+//   let greatestWord = null;
+//   let greatestValue = 0;
+//   for (let i = 0; i < words.length; i++) {
+//     const value = wordValue(words[i]);
+//     if (value > greatestValue) {
+//       greatestValue = value;
+//       greatestWord = words[i];
+//     }
+//   }
+//   return greatestWord;
+// }
+// const words = ["hello", "world", "javascript", "programming"];
+// const greatestWord = findGreatesValue(words);
+// console.log(`The word with the biggest num is ${greatestWord}`);
+
+function high(x) {
+  let lettersToNumbers = x.toUpperCase().charCodeAt(0) - 64;
+
+  // let numArray = [];
+  //   for (let i = 0; i < lettersToNumbers.length; i++) {
+  //     numArray.push(lettersToNumbers[i]);
+  //   }
+
+  return lettersToNumbers;
 }
-const words = ["hello", "world", "javascript", "programming"];
-const greatestWord = findGreatesValue(words);
-console.log(`The word with the biggest num is ${greatestWord}`);
+console.log(high("a b c"));
+// const words = "ab c";
+// const arraty = [];
+// for (let i = 0; i < words.length; i++) {
+//   arraty.push(words[i]);
+// }
+// console.log(arraty);
