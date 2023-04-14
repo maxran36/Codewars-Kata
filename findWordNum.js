b68da2fccf1d3033602345ca21d419a7a3c744d6
maxran36 <124910435+maxran36@users.noreply.github.com>
2023-04-14 18:10:21 +0000

    made seperate file for WordNum kata

---
+++ findWordNum.js
@@ -0,0 +1,88 @@
+// function high(x) {
+//   let result = 0;
+//   let letterArray = [];
+//   let charCode;
+//   letterArray = x.split(" ");
+
+//   console.log(letterArray);
+//   for (let i = 0; i < x.length; i++) {
+//     if (x.charCodeAt(i) >= 97 && x.charCodeAt(i) <= 122) {
+//       charCode = x.charCodeAt(i) - 96;
+//       result += charCode;
+//     }
+//   }
+//   return result;
+// }
+// console.log(high("abc abcd"));
+// const strArray = "aa aa";
+// let splitArray = strArray.split(" ");
+// let countArray = [];
+
+// splitArray = String(splitArray);
+// // console.log(typeof splitArray);
+// // console.log(typeof strArray);
+// for (let i = 0; i < splitArray.length; i++) {
+//   countArray.push(splitArray.charCodeAt(i) - 96);
+// }
+// //console.log(countArray.slice(0, 3));
+
+// let newArray = countArray.filter((val) => val > 0);
+// console.log(countArray);
+// let sum = 0;
+// let sum2 = 0;
+// countArray.reduce((acc, num) => {
+//   if (num > 0) {
+//     sum += num;
+//   } else sum2 += num;
+// }, 0);
+// console.log(sum);
+// const letterValues = {
+//   A: 1,
+//   B: 2,
+//   C: 3,
+//   D: 4,
+// };
+// function findHighestLetterValue(str) {
+//   let highestValue = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     const letter = str[i].toUpperCase();
+//     const value = letterValues[letter];
+//     if (value > highestValue) {
+//       highestValue = value;
+//     }
+//   }
+//   return highestValue;
+// }
+// const inputString = "aa bb dc";
+// const highestValue = findHighestLetterValue(inputString);
+// console.log(highestValue);
+
+// Assign numbers to letter
+function letterValues(char) {
+  return char.toUpperCase().charCodeAt(0) - 64;
+}
+
+// Function to calculate the value of a word based on the sum of it's letter values
+function wordValue(word) {
+  let value = 0;
+  for (let i = 0; i < word.length; i++) {
+    value += letterValues(word.charAt(i));
+  }
+  return value;
+}
+
+function findGreatesValue(words) {
+  let greatestWord = null;
+  let greatestValue = 0;
+  for (let i = 0; i < words.length; i++) {
+    const value = wordValue(words[i]);
+    if (value > greatestValue) {
+      greatestValue = value;
+      greatestWord = words[i];
+    }
+  }
+  return greatestWord;
+}
+const words = ["hello", "world", "javascript", "programming"];
+const greatestWord = findGreatesValue(words);
+console.log(`The word with the biggest num is ${greatestWord}`);
