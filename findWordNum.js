898d3481374ab1f6609c9af08c6e21875f1575f2
maxran36 <124910435+maxran36@users.noreply.github.com>
2023-04-18 15:38:48 +0000

    Final code for Highest Word Kata

--- findWordNum.js
+++ findWordNum.js
@@ -57,19 +57,30 @@
 // const highestValue = findHighestLetterValue(inputString);
 // console.log(highestValue);
 
-// Assign numbers to letter
+// //FUNCTION TO MAKE SENTENCE ARRAY
+// function sentenceToArray(str) {
+//   const wordArray = str.split(" ");
+//   return wordArray;
+// }
+// console.log(sentenceToArray("ab bb "));
+// //Assign numbers to letter
 // function letterValues(char) {
+//   sentenceToArray(char);
 //   return char.toUpperCase().charCodeAt(0) - 64;
 // }
+// console.log(letterValues("ab is very high letter"));
+// //Function to calculate the value of a word based on the sum of it's letter values
 
-// // Function to calculate the value of a word based on the sum of it's letter values
+// //CHECK FRIDAY"S CODE TO SEE WHAT I CHANGED
 // function wordValue(word) {
+//   word = sentenceToArray(word);
 //   let value = 0;
 //   for (let i = 0; i < word.length; i++) {
 //     value += letterValues(word.charAt(i));
 //   }
 //   return value;
 // }
+// console.log(wordValue("ab bb"));
 
 // function findGreatesValue(words) {
 //   let greatestWord = null;
@@ -83,24 +94,75 @@
 //   }
 //   return greatestWord;
 // }
-// const words = ["hello", "world", "javascript", "programming"];
+//const words = ["hello", "world", "javascript", "programming"];
+
 // const greatestWord = findGreatesValue(words);
 // console.log(`The word with the biggest num is ${greatestWord}`);
 
-function high(x) {
-  let lettersToNumbers = x.toUpperCase().charCodeAt(0) - 64;
+// function high(x) {
+//   let lettersToNumbers = x.toUpperCase().charCodeAt(0) - 64;
 
-  // let numArray = [];
-  //   for (let i = 0; i < lettersToNumbers.length; i++) {
-  //     numArray.push(lettersToNumbers[i]);
-  //   }
+// let numArray = [];
+//   for (let i = 0; i < lettersToNumbers.length; i++) {
+//     numArray.push(lettersToNumbers[i]);
+//   }
 
-  return lettersToNumbers;
-}
-console.log(high("a b c"));
+//   return lettersToNumbers;
+// }
+//////console.log(high("a b c"));
 // const words = "ab c";
 // const arraty = [];
 // for (let i = 0; i < words.length; i++) {
 //   arraty.push(words[i]);
 // }
 // console.log(arraty);
+
+// function highestWord(sentence) {
+//   const words = sentence.split(" ");
+//   let highestScore = 0;
+//   let highestWord = "";
+
+//   for (let i = 0; i < words.length; i++) {
+//     const word = words[i];
+//     let score = 0;
+
+//     for (let j = 0; j < word.length; j++) {
+//       const letter = word[j].toUpperCase();
+//       score += letter.charCodeAt(0) - 64;
+//     }
+//     if (score > highestScore) {
+//       highestScore = score;
+//       highestWord = word;
+//     }
+//   }
+//   return highestWord;
+// }
+// console.log(highestWord("a bb ccc z"));
+
+
+//Solution that worked
+function high(x) {
+  //Takes sentence string and turns it into an array
+  const words = x.split(" ");
+  let highestScore = 0;
+  let highestWord = "";
+  // Main loop through word array
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let score = 0;
+
+    //assigns numbers to letters and sums
+    for (let j = 0; j < word.length; j++) {
+      const letter = word[j].toUpperCase();
+      score += letter.charCodeAt(0) - 64;
+    }
+
+    //Takes the sum of each word at the loop iteration and see if it is higher than the previous
+    if (score > highestScore) {
+      highestScore = score;
+      highestWord = word;
+    }
+  }
+  return highestWord;
+}
+console.log(high("abcd zpp"));
